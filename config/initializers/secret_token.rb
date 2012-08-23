b88eef8682c034b77374bf6b64f3f450d30fedb6
Onur Uyar <fluxproject@gmail.com>
2012-08-23 16:02:41 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampanifyWeb::Application.config.secret_token = 'aa4580497065543b6303c641cd9c3da99d458a3324311a36677a8b6c67348b1e534ef8430ffe7e27a7360b89eb00174a56e272b0156e1d18a498a016b82bf8a0'

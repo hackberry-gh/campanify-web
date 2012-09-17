0d026b53ee84d811bf9ad575e06949587f35257c
Onur Uyar <fluxproject@gmail.com>
2012-09-17 18:10:12 +0100

    return to thin, remove capistrano

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = ::File.expand_path('../..',  __FILE__)
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.blog.sock"
-worker_processes 2
-timeout 30

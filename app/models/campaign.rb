0e2513f77c7f1473ba1e326a400dba91151fd5cc
Onur Uyar <me@onuruyar.com>
2013-06-03 15:15:00 +0100

    falan

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -17,7 +17,13 @@ class Campaign < ActiveRecord::Base
   before_update     :change_theme  
   before_destroy    :destroy_app
   
-  attr_accessor  :migration_steps
+  def migration_steps
+    @migration_steps ||= []
+  end
+  
+  def migration_steps=ms
+    self.migration_steps = ms
+  end
   
   def serializable_hash(options = {})
     super((options || { }).merge({
